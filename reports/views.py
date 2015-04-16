cb74220686338fee8a3aed4a454d20a5d946bc19
ankitimaginea <ankit.singh@imaginea.com>
2015-04-16 18:12:28 +0530

    icinga django report added

--- reports/views.py
+++ reports/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
+from extractor.processor import Processor
 # Create your views here.
 
 
 def index(request):
-    context = {'latest_question_list': 'latest_question_list'}
-    return render(request, 'reports/index.html', context)
+    processor = Processor()
+    formatted_rows = processor.format()
+    header = formatted_rows[0]
+    formatted_rows = formatted_rows[1:]
+    # context = {'latest_question_list': 'latest_question_list'}
+    return render(request, 'reports/index.html', locals())
